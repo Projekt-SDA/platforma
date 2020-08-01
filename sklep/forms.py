f5d5149b17b8c9a055e6d06607968d3505a9f137
Damian <pcpatrol@o2.pl>
2020-08-01 10:42:51 +0200

    Add new Files

---
+++ sklep/forms.py
@@ -0,0 +1,15 @@
+from django.forms import Form
+from .models import Product
+from django.db.models import (
+ CharField, DecimalField, TextField
+)
+
+
+class ProductForms(Form):
+    nazwa = CharField(max_length=100)
+    code = CharField(max_length=13)
+    cena = DecimalField(max_digits=9, decimal_places=2)
+    opis = TextField(default="Proszę o dodanie opisu (opcjonalnie)", null=True)
+
+
+
---
+++ sklep/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .forms import ProductForms
+from django.views.generic import FormView, ListView
+from django.shortcuts import render
+
+from .models import Product
+
+def home_view(request, *args, **kwargs):
+    return render(request, "index.html", {})
+
+
+def stan_sklepu(request, *args, **kwargs):
+    return render(request, "sklep/stan_sklepu.html", {})
+
+
+def pasek_nawigacyjny_sklep(request, *args, **kwargs):
+    return render(request, "sklep/pasek_nawigacyjny_sklep.html", {})
+
+def dodaj_produkt_sklep(request, *args, **kwargs):
+    return render(request, "sklep/dodaj.html", {})
+
+
+
+
+def products(request):
+    return render(
+        request, template_name='stan_sklepu.html',
+        context={'product':Product.odjects.all()}
+    )
+
+def produkt_opis_widok(request):
+    obj = Product.objects.all()
+    kontekst = {
+         'obiekt': obj
+
+    }
+    return render(request, "sklep/stan_sklepu.html", kontekst)
+
+
